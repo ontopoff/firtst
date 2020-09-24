b93692e6d64f9708de2a5a9171375e7d5c2b90f9
ontopoff <avenue200@yandex.ru>
2020-09-24 15:05:48 +0300

    Хотя бы работает

---
+++ src/Banknotes.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Banknotes {
+
+    static long exchange(long value, long bankM[], int i, long denomination[]) {
+        long temp = 0;
+        value -= bankM[i];
+        ++denomination[i];
+        if(value == 0) {
+                for(int j = 0; j < denomination.length; ++j) {
+                    if(denomination[j] != 0) {
+                        for(int k = 0; k < denomination[j]; ++k) System.out.print(bankM[j] + " ");
+                    }
+                }
+                System.out.println("");
+                return 1;
+        }
+        for(int j = i; j < bankM.length; ++j) {
+            if(value - bankM[j] >= 0) {
+                temp += exchange(value, bankM, j, denomination);
+                --denomination[j];
+            }
+        }
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        long res = 0;
+        Scanner myObj = new Scanner(System.in);
+        long value = myObj.nextLong();
+        int n = myObj.nextInt();
+        long bankM[] = new long[n];
+        long denomination[] = new long[n];
+        for(int i = 0; i < bankM.length; ++i) {
+            bankM[i] = myObj.nextLong();
+        }
+        for(int i = 0; i < bankM.length; ++i) {
+            if(value - bankM[i] >= 0) {
+                res += exchange(value, bankM, i, denomination);
+                --denomination[i];
+            }
+        }
+        System.out.println(res);
+    }
+}
