19700ef89277b757783d0ba2beeedc02c3d5dd5b
ontopoff <avenue200@yandex.ru>
2020-11-05 17:22:29 +0300

    List v1.0

--- src/com/ontopoff/collections/ListClass.java
+++ src/com/ontopoff/collections/ListClass.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.Arrays;
 
 public class ListClass implements java.util.List {
 
@@ -13,11 +14,12 @@ public class ListClass implements java.util.List {
     Object[] data;
     private int cnt;
 
-    ListClass() {
-        size = 0;
+    ListClass(int size) {
+        this.size = size;
         next = new int[size+1];
-        head = 0;
-        cnt = 1;
+        Arrays.fill(next, -1);
+        head = -1;
+        cnt = 0;
         data = new Object[size+1];
     }
 
@@ -28,181 +30,231 @@ public class ListClass implements java.util.List {
 
     @Override
     public boolean isEmpty() {
-        return this.head == 0;
+        return this.head == -1;
     }
 
     @Override
-    public boolean contains(Object o) {
+    public boolean contains(Object value) {
+        if(cnt == 0) {
+            return false;
+        }
+        int currentId = this.head;
+        for(int listId = 0; listId < this.cnt; ++listId) {
+            if(data[currentId] == value) {
+                return true;
+            } else {
+                currentId = next[currentId];
+            }
+        }
         return false;
     }
 
     @Override
     public Iterator iterator() {
-        return null;
+        throw new UnsupportedOperationException("Этот метод не реализован!");
     }
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        throw new UnsupportedOperationException("Этот метод не реализован!");
     }
 
     @Override
-    public boolean add(Object o) {
-        return false;
+    public boolean add(Object value) {
+        int listId = this.head;
+        if(listId == -1) {
+            this.head = 0;
+            data[0] = value;
+            cnt++;
+        } else {
+            while (next[listId] != -1) {
+                listId = next[listId];
+            }
+            next[listId] = ++listId;
+            data[listId] = value;
+            cnt++;
+        }
+        return true;
     }
 
     @Override
     public boolean remove(Object o) {
-        return false;
+        throw new UnsupportedOperationException("Этот метод не реализован!");
     }
 
     @Override
     public boolean addAll(Collection c) {
-        return false;
+        throw new UnsupportedOperationException("Этот метод не реализован!");
     }
 
     @Override
     public boolean addAll(int index, Collection c) {
-        return false;
+        throw new UnsupportedOperationException("Этот метод не реализован!");
     }
 
     @Override
     public void clear() {
-
+        throw new UnsupportedOperationException("Этот метод не реализован!");
     }
 
     @Override
     public Object get(int index) {
-        int listCurr = this.head;
-        for(int listId = 0; listId <= index; ++listId) {
-            listCurr = next[listId];
+        if(index < 0) {
+            throw new ArrayIndexOutOfBoundsException("Список не поддерживает отрицательные индексы");
+        } else if(index >= size) {
+            throw new ArrayIndexOutOfBoundsException("Индекс искомого элемента превосходит количество элементов списка");
+        } if(index >= cnt) {
+            throw new ArrayIndexOutOfBoundsException("Элемент с таким индексом не найден");
+        } else {
+            int listId = this.head;
+            int foundId = -1;
+            if ((next[listId] == -1) || (index == 0)) {
+                return data[listId];
+            } else {
+                for (int i = 0; i < index; ++i) {
+                    foundId = next[listId];
+                    listId = foundId;
+                }
+            }
+            return data[foundId];
         }
-        return data[listCurr];
     }
 
     @Override
     public Object set(int index, Object element) {
-        return null;
+        throw new UnsupportedOperationException("Этот метод не реализован!");
     }
 
     @Override
-    public void add(int head, Object v) {
-        if(head == data.length) {
-            size++;
-            Object[] newData = new Object[size + 1];
-            System.arraycopy(data, 0, newData, 1, data.length);
-            data = newData;
-            int[] newNext = new int[size + 1];
-            System.arraycopy(next, 0, newNext, 1, next.length);
-            next = newNext;
+    public void add(int index, Object value) {
+        if(index < 0) {
+            throw new ArrayIndexOutOfBoundsException("Список не поддерживает отрицательные индексы");
+        } else if((index > cnt) || (index >= size)) {
+            throw new ArrayIndexOutOfBoundsException("Индекс вставляемого элемента превосходит размеры списка");
+        } else if(this.head == -1) {
+            this.head = 0;
+            data[0] = value;
+            cnt++;
+        } else {
+            int listId = this.head;
+            int headId = listId;
+            int beforePasteId = listId, pasteId = listId;
+            int beforeId = listId, foundId;
+
+            if (index == cnt) {
+                while (next[listId] != -1) {
+                    listId = next[listId];
+                }
+                next[listId] = ++listId;
+                data[listId] = value;
+                next[listId] = -1;
+                cnt++;
+            } else {
+                while (next[listId] != -1) {
+                    beforeId = listId;
+                    listId = next[listId];
+                }
+                foundId = listId;
+                ++listId;
+                data[listId] = data[foundId];
+                next[foundId] = -1;
+                next[listId] = -1;
+                next[beforeId] = listId;
+                if(cnt == 1) {
+                    headId = listId;
+                }
+
+                if (index == 0) {
+                    this.head = foundId;
+                    next[foundId] = headId;
+                    data[foundId] = value;
+                    cnt++;
+                } else {
+                    for (int i = 0; i < index; ++i) {
+                        beforePasteId = pasteId;
+                        pasteId = next[pasteId];
+                    }
+                    next[foundId] = next[beforePasteId];
+                    next[beforePasteId] = foundId;
+                    data[foundId] = value;
+                    cnt++;
+                }
+            }
         }
-        next[0] = head;
-        data[0] = v;
-        this.head = cnt++;
     }
 
     @Override
     public Object remove(int index) {
-        if(this.head == 0) {
-            return null;
+        if(index < 0) {
+            throw new ArrayIndexOutOfBoundsException("Список не поддерживает отрицательные индексы");
+        } else if(index >= size) {
+            throw new ArrayIndexOutOfBoundsException("Индекс удаляемого элемента превосходит размеры списка");
+        } if(index >= cnt) {
+            throw new ArrayIndexOutOfBoundsException("Элемент с таким индексом не найден и не может быть удален");
+        } else {
+            int listCurr = this.head;
+                if (index == 0) {
+                    System.out.println("Удаляемое значение: " + data[listCurr]);
+                    this.head = next[listCurr];
+                    next[listCurr] = -1;
+                    cnt--;
+                } else {
+                    int foundId = listCurr, beforeId = listCurr;
+                    for (int i = 0; i < index; ++i) {
+                        beforeId = foundId;
+                        foundId = next[foundId];
+                    }
+                    System.out.println("Удаляемое значение: " + data[foundId]);
+                    next[beforeId] = next[foundId];
+                    next[foundId] = -1;
+                    cnt--;
+                }
+            return data[listCurr];
         }
-        int listCurr = this.head;
-        for(int listId = 0; listId <= index; ++listId) {
-            listCurr = next[listId];
-            next[listId]--;
-        }
-        System.out.println("Удаляемое значение: " + data[listCurr]);
-        Object[] newData = new Object[size];
-
-        int iterListId = 0, listId = 0;
-        while (iterListId < data.length) {
-            if(iterListId != listCurr) {
-                newData[listId] = data[iterListId];
-                listId++;
-            }
-            iterListId++;
-        }
-        data = newData;
-        int[] newNext = new int[size];
-
-        iterListId = 0;
-        listId = 0;
-        while (iterListId < data.length) {
-            if(iterListId != listCurr) {
-                newNext[listId] = next[iterListId];
-                listId++;
-            }
-            iterListId++;
-        }
-        next = newNext;
-        size--;
-        head = --cnt;
-        for (int i = 0; i < next.length; ++i) {
-            System.out.println(next[i]);
-        }
-        return null;
     }
 
     @Override
     public int indexOf(Object o) {
-        return 0;
+        throw new UnsupportedOperationException("Этот метод не реализован!");
     }
 
     @Override
     public int lastIndexOf(Object o) {
-        return 0;
+        throw new UnsupportedOperationException("Этот метод не реализован!");
     }
 
     @Override
     public ListIterator listIterator() {
-        return null;
+        throw new UnsupportedOperationException("Этот метод не реализован!");
     }
 
     @Override
     public ListIterator listIterator(int index) {
-        return null;
+        throw new UnsupportedOperationException("Этот метод не реализован!");
     }
 
     @Override
     public List subList(int fromIndex, int toIndex) {
-        return null;
+        throw new UnsupportedOperationException("Этот метод не реализован!");
     }
 
     @Override
     public boolean retainAll(Collection c) {
-        return false;
+        throw new UnsupportedOperationException("Этот метод не реализован!");
     }
 
     @Override
     public boolean removeAll(Collection c) {
-        return false;
+        throw new UnsupportedOperationException("Этот метод не реализован!");
     }
 
     @Override
     public boolean containsAll(Collection c) {
-        return false;
+        throw new UnsupportedOperationException("Этот метод не реализован!");
     }
 
     @Override
     public Object[] toArray(Object[] a) {
-        return new Object[0];
-    }
-
-    public static void main(String[] args) {
-        ListClass List = new ListClass();
-        List.add(List.head, 1);
-        List.add(List.head, 2);
-        List.add(List.head, 3);
-        List.add(List.head, 4);
-        System.out.println(List.data[0]);
-        System.out.println(List.data[1]);
-        System.out.println(List.data[2]);
-        System.out.println(List.data[3]);
-        System.out.println(List.get(0));
-        System.out.println(List.get(1));
-        System.out.println(List.get(3));
-        List.remove(1);
-        List.remove(2);
-        List.remove(0);
+        throw new UnsupportedOperationException("Этот метод не реализован!");
     }
 }
