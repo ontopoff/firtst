922c7950110eaa20e2b64cfaa6f3aa35c1d553d6
ontopoff <avenue200@yandex.ru>
2020-12-02 18:48:43 +0300

    thirdTask fixes(removed empty checks and System.out.println)

--- src/com/ontopoff/structures/HashMapClass.java
+++ src/com/ontopoff/structures/HashMapClass.java
@@ -18,12 +18,6 @@ public class HashMapClass implements java.util.Map {
         nullKeyValue = null;
     }
 
-
-    @Override
-    public int size() {
-        throw new UnsupportedOperationException("Этот метод не реализован!");
-    }
-
     @Override
     public boolean isEmpty() {
         return (this.cnt == 0) ? true : false;
@@ -47,17 +41,11 @@ public class HashMapClass implements java.util.Map {
     }
 
     @Override
-    public boolean containsValue(Object value) {
-        throw new UnsupportedOperationException("Этот метод не реализован!");
-    }
-
-    @Override
     public Object get(Object key) {
         if(key == null) {
             if(nullKey) {
                 return nullKeyValue;
             } else {
-                System.out.println("Элемент с key = "+ key + " не был найден!");
                 return null;
             }
         } else {
@@ -72,25 +60,22 @@ public class HashMapClass implements java.util.Map {
     @Override
     public Object put(Object key, Object value) {
         if(key == null) {
-            if(!nullKey) {
-                System.out.println("Элемент с key = null не был найден!");
-                System.out.println("На его место будет записано значение: "+ value);
-            } else {
-                System.out.println("Элементу с key = null будет перезаписано значение: "+ value);
-            }
             nullKey = true;
             nullKeyValue = value;
         } else {
             if (hashMap.contains(key)) {
-                System.out.println("Элемент с key = "+ key +" уже записан. Его значение будет перезаписано на: "+ value);
                 hashMap.put(key, value);
             } else {
-                System.out.println("На его место будет записано значение: "+ value);
                 hashMap.put(key, value);
                 cnt++;
             }
         }
         return null;
+    }
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("Этот метод не реализован!");
     }
 
     @Override
@@ -120,6 +105,11 @@ public class HashMapClass implements java.util.Map {
 
     @Override
     public Set<Entry> entrySet() {
+        throw new UnsupportedOperationException("Этот метод не реализован!");
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
         throw new UnsupportedOperationException("Этот метод не реализован!");
     }
 }
--- src/com/ontopoff/structures/HashTableClass.java
+++ src/com/ontopoff/structures/HashTableClass.java
@@ -11,6 +11,7 @@ public class HashTableClass<K, V> {
         public LinkedListItem<K, V> prev;
         public K key;
         public V value;
+
         LinkedListItem(K key, V value) {
             this.key = key;
             this.value = value;
@@ -27,94 +28,63 @@ public class HashTableClass<K, V> {
 
     public V put(K key, V value) {
         LinkedListItem<K, V> node = getNode(key);
-        try {
-            if(!node.equals(null)) {
-                V copyValue = node.value;
-                node.value = value;
-                return copyValue;
-            }
-        } catch (NullPointerException e) {}
+        if (node != null) {
+            V copyValue = node.value;
+            node.value = value;
+            return copyValue;
+        }
         node = new LinkedListItem<K, V>(key, value);
         int index = getIndex(key);
-        try {
-            if(!hashTable.get(index).equals(null)) {
-                node.next = hashTable.get(index);
-                node.next.prev = node;
-            }
-        } catch (NullPointerException e) {}
+        if (hashTable.get(index) != null) {
+            node.next = hashTable.get(index);
+            node.next.prev = node;
+        }
         hashTable.set(index, node);
         return null;
     }
 
     public V remove(K key) {
         LinkedListItem<K, V> node = getNode(key);
-        try {
-            if(node.equals(null)) {
-            }
-        } catch (NullPointerException e) {
+        if (node == null) {
             throw new NullPointerException("Элемент с таким ключом не был найден и не может быть удален");
         }
-        try {
-            if(!node.prev.equals(null)) {
-                node.prev.next = node.next;
-            }
-        } catch (NullPointerException e) {
+        if (node.prev != null) {
+            node.prev.next = node.next;
+        } else {
             int hash = getIndex(key);
             hashTable.set(hash, node.next);
         }
-        try {
-            if (!node.next.equals(null)) {
-                node.next.prev = node.prev;
-            }
-        } catch (NullPointerException e) {}
+        if (node.next != null) {
+            node.next.prev = node.prev;
+        }
         return node.value;
     }
 
     public boolean contains(K key) {
-        try {
-            if(key.equals(null)) {}
-        } catch (NullPointerException e) {
+        if (key == null) {
             throw new NullPointerException("Ключ не поддерживает значение null");
         }
         LinkedListItem<K, V> node = getNode(key);
-        try {
-            if(node.equals(null)) {} else {
-                return true;
-            }
-        } catch (NullPointerException e) {
-            System.out.println("Элемент с key = "+ key + " не был найден!");
-        }
-        return false;
+        return node == null ? false : true;
     }
 
     public V get(K key) {
-        try {
-            if(key.equals(null)) {}
-        } catch (NullPointerException e) {
+        if (key == null) {
             throw new NullPointerException("Ключ не поддерживает значение null");
         }
         LinkedListItem<K, V> node = getNode(key);
-        try {
-            if(node.equals(null)) {} else {
-                return node.value;
-            }
-        } catch (NullPointerException e) {
-            System.out.println("Элемент с key = "+ key + " не был найден!");
-        }
-        return null;
+        return node == null ? null : node.value;
     }
 
     private LinkedListItem<K, V> getNode(K key) {
         int index = getIndex(key);
         LinkedListItem<K, V> currItem = hashTable.get(index);
-        try {
-            while (!currItem.equals(null)) {
-                if(currItem.key.equals(key)) {
-                    return currItem;
-                }
-                currItem = currItem.next;
+        while (currItem != null) {
+            if (currItem.key.equals(key)) {
+                return currItem;
             }
-        } catch (NullPointerException e) {}
+            currItem = currItem.next;
+        }
         return null;
     }
 
